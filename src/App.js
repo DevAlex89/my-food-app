3fce8c6de65b5f3ea054de559c1a574e4300b4dd
Alexandros-Rezkalla <alexandrosrezk@gmail.com>
2022-01-18 10:55:25 +0200

    starting on homepage

--- src/App.js
+++ src/App.js
@@ -3,36 +3,51 @@ import {
   ChakraProvider,
   Box,
   Text,
+  Heading,
+  Button,
+  Container,
+  Image,
+  Center,
   Link,
   VStack,
   Code,
   Grid,
   theme,
+  Flex,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import logo from './assets/logo.png'
+
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+      <Box minH='100vh' w='auto'>
+        <Grid w='100vw'>
+           <Flex justify='space-between' align='center' w='90vw'  ml='auto' mr='auto' minH='20vh'>
+             <Image boxSize='80px' src={logo}/>
+             <ColorModeSwitcher p='8'  border='1px'/>
+           </Flex>
+           <Box h='80vh'>
+             <Container>
+               <Heading align='center' fontSize='6xl'>Hello World</Heading>
+               <Text align='center' fontSize='xl' mt='3'>We dream of a planet with no food waste, and every day we’re working on making that a reality. Log on, and get saving perfectly good, surplus food from your local stores. It’s always a surprise, at a great price, and an instant good deed for the planet. Get started now!</Text>
+             </Container>
+             <Box mt='12' w='90vw' display='flex' justify='space-between' ml='auto' mr='auto' bg='tomato' align='center'>
+               <Container>
+                 <Heading fontSize='2xl'>For the foodies</Heading>
+                 <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus enim neque, aliquam laudantium blanditiis veritatis.</Text>
+                 <Button>Sign In</Button>
+               </Container>
+               <Container>
+                 <Heading fontSize='2xl'>For the businesses</Heading>
+                 <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia expedita tempora quo aliquam, ab hic.</Text>
+                 <Button>Sign In</Button>
+               </Container>
+             </Box>
+             
+           </Box>
         </Grid>
       </Box>
     </ChakraProvider>
