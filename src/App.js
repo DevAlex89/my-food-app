16f011d8e5da0c5a2a22e9721ea8e87b94ed4257
Alexandros-Rezkalla <alexandrosrezk@gmail.com>
2022-01-24 21:13:27 +0200

    working on adding to firebase db and will try figuring geohash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ChakraProvider,theme } from '@chakra-ui/react';
 import {BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home'
 import UserLogIn from './components/UserLogIn';
 import BusinessLogIn from './components/BusinessLogIn';
@@ -13,7 +12,6 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Router>
-        <Navbar />
         <Routes>
           <Route  path='/' element={<Home/>} />
           <Route path='/UserLogIn' element={<UserLogIn/>} />
--- src/components/BusinessLogIn.js
+++ src/components/BusinessLogIn.js
@@ -1,26 +1,35 @@
 import { Container , Box , Heading , FormControl , FormLabel ,Input , Button , Text} from '@chakra-ui/react';
-import React from 'react';
-import { useState } from 'react';
+import React ,{useState} from 'react';
 import PlacesAutocomplete ,{geocodeByAddress,getLatLng} from 'react-places-autocomplete';
+import {db} from './firebase-config';
+import {collection ,addDoc} from 'firebase/firestore';
 
 
 function BusinessLogIn() {
    const [address,setAddress] = useState('');
    const [coordinates, setCoordinates] = useState({lat:null , lng:null});
+   const [business , setBusiness] = useState({});
+   const [businessCollectionRef] = collection(db , 'Businesses')
+   const [newName , setNewName] = useState('');
+   const [newBags , setNewBags] = useState(0)
 
 
 
     // the function to get coords from address
-   const handleSelect = async value =>{
+   const handleSelect = async value => {
      const results = await geocodeByAddress(value);
      const latLng = await getLatLng(results[0]);
      setAddress(value);
      setCoordinates(latLng);
-     console.log(coordinates)
    };
 
+  //  the function to add new business
+    const registerNewBusiness = async () => {
+        await addDoc(businessCollectionRef , {Name:newName , FoodBags: newBags , Location: coordinates , Geohash:?})
+    }
 
-  return <Container maxW='container.xl'  centerContent>
+
+  return <Container maxW='container.xl'  centerContent mt={24}>
     <Box>
       <Heading align='center' mt={4} mb={4}>Sign up your business and save food waste</Heading>
       <Text mb={8}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates rerum nesciunt similique eveniet ipsam! Earum quaerat accusamus ab modi hic.</Text>
@@ -28,7 +37,7 @@ function BusinessLogIn() {
 
 
     {/* heres the autocomplete input for location */}
-    <FormControl maxW='lg'>
+    <FormControl maxW='lg' mt={12}>
         <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
            {({ getInputProps, suggestions, getSuggestionItemProps, loading })=>
               <Box>
@@ -45,10 +54,10 @@ function BusinessLogIn() {
             }
          </PlacesAutocomplete>
          <FormLabel>Business Name</FormLabel>
-         <Input type='text' placeholder='Brand name'/>
+         <Input type='text' placeholder='Brand name' onChange={(e)=>{setNewName(e.target.value)}}/>
          <FormLabel>Available Food Bags</FormLabel>
-         <Input type='number' placeholder='*requires number'/>
-         <Button mt={4} type='submit' w='100%'>Register your goods</Button>
+         <Input type='number' placeholder='*requires number' onChange={(e)=>{setNewBags(e.target.value)}}/>
+         <Button mt={4} type='submit' w='100%' onClick={registerNewBusiness}>Register your goods</Button>
     </FormControl>
     <Box>
       <Text>{coordinates.lat}</Text>
