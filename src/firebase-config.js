2044ba077ecffd4896aa0d7c0553ea0f8bdee885
Alexandros-Rezkalla <alexandrosrezk@gmail.com>
2022-01-24 19:55:59 +0200

    added firebase & config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,15 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfbevVuXFCmPWS0bUc7jYLFy_DghanNHI",
-    authDomain: "save-food-app.firebaseapp.com",
-    projectId: "save-food-app",
-    storageBucket: "save-food-app.appspot.com",
-    messagingSenderId: "49052333865",
-    appId: "1:49052333865:web:3acd44d98f3535a305e938",
-    measurementId: "G-SRBV9YTDHQ"
-  };
+  apiKey: 'AIzaSyCfbevVuXFCmPWS0bUc7jYLFy_DghanNHI',
+  authDomain: 'save-food-app.firebaseapp.com',
+  projectId: 'save-food-app',
+  storageBucket: 'save-food-app.appspot.com',
+  messagingSenderId: '49052333865',
+  appId: '1:49052333865:web:3acd44d98f3535a305e938',
+  measurementId: 'G-SRBV9YTDHQ',
+};
 
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
