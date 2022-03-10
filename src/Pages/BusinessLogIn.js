c7d105e0eef40aa5343b343418cc7a6864a89f50
Alexandros-Rezkalla <alexandrosrezk@gmail.com>
2022-03-10 18:27:50 +0200

    fixed login button stuck on loading

--- src/Pages/BusinessLogIn.js
+++ src/Pages/BusinessLogIn.js
@@ -10,82 +10,83 @@ import {
   Flex,
   Spacer,
   Text,
-  Link
+  Link,
 } from '@chakra-ui/react';
-import React, { useState , useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { useAuthContext } from '../contexts/AuthContext';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const BusinessLogin = () => {
   let navigate = useNavigate();
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-  const {login, forgotPassword, setLocal} = useAuthContext()
-  const [message, setMessage] = useState('')
-  
-  
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { login, forgotPassword, setLocal } = useAuthContext();
+  const [message, setMessage] = useState('');
 
+  // the user login
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setError('');
+    setLoading(true);
+    try {
+      if (emailRef && passwordRef)
+        await login(emailRef.current.value, passwordRef.current.value).then(
+          userCred => {
+            setLocal();
 
-// the user login
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-      setError('')
-      setLoading(true)
-      try{
-        if (emailRef && passwordRef)
-        await login( emailRef.current.value, passwordRef.current.value)
-        .then ((userCred)=>{
-          
-          setLocal()
-
-          navigate('/Dashboard')
-        })
-        setLoading(false)
-    }catch{
-      setError('Wrong email or password')
+            navigate('/Dashboard');
+          }
+        );
+      setLoading(false);
+    } catch {
+      setError('Wrong email or password');
+      setLoading(false);
     }
-  }
+  };
 
   // reset password
   const forgotPasswordHandler = async () => {
-    try{
-      if (emailRef.current.value) 
-      await forgotPassword(emailRef.current.value).then(() => {
-        emailRef.current.value = "";
-      });
-      setMessage('An email has been sent with instructions to reset your password')
-    } catch (err){
-      setError('Wrong Email')
+    try {
+      if (emailRef.current.value)
+        await forgotPassword(emailRef.current.value).then(() => {
+          emailRef.current.value = '';
+        });
+      setMessage(
+        'An email has been sent with instructions to reset your password'
+      );
+    } catch (err) {
+      setError('Wrong Email');
     }
   };
-  
 
   return (
-    <Container minH='76vh'>
-      <Heading align='center' color='#114d4d' mt={5} mb={2} >Log In</Heading>
+    <Container minH="76vh">
+      <Heading align="center" color="#114d4d" mt={5} mb={2}>
+        Log In
+      </Heading>
       {error && <Alert status="error">{error}</Alert>}
       {message && <Alert status="success">{message}</Alert>}
-      <FormControl  >
-        <FormLabel color='#114d4d'>Email</FormLabel>
+      <FormControl>
+        <FormLabel color="#114d4d">Email</FormLabel>
         <Input
           focusBorderColor="#114d4d"
           type="email"
-          placeholder='Email'
+          placeholder="Email"
           ref={emailRef}
           required
         />
-        <FormLabel color='#114d4d'>Password</FormLabel>
+        <FormLabel color="#114d4d">Password</FormLabel>
         <Input
           focusBorderColor="#114d4d"
           type="password"
-          placeholder='Requires at least 6 characters'
+          placeholder="Requires at least 6 characters"
           ref={passwordRef}
           required
         />
       </FormControl>
-      <Button 
+      <Button
         disabled={loading}
         color="#114d4d"
         variant="ghost"
@@ -96,14 +97,26 @@ const BusinessLogin = () => {
         type="submit"
         w="100%"
         onClick={handleSubmit}
-        >Log In</Button>
-        <Flex mt={5} flexDir={['column','column','row','row']}>
-          <Box>
-          Need an account? <Link ml={2} color='#49ada1' href="/BusinessRegister">Sign Up</Link>
-          </Box>
-          <Spacer />
-          <Text cursor='pointer' _hover={{textDecor:'underline'}} color='#49ada1' onClick={forgotPasswordHandler}>Forgot your password?</Text>
-        </Flex>
+      >
+        Log In
+      </Button>
+      <Flex mt={5} flexDir={['column', 'column', 'row', 'row']}>
+        <Box>
+          Need an account?{' '}
+          <Link ml={2} color="#49ada1" href="/BusinessRegister">
+            Sign Up
+          </Link>
+        </Box>
+        <Spacer />
+        <Text
+          cursor="pointer"
+          _hover={{ textDecor: 'underline' }}
+          color="#49ada1"
+          onClick={forgotPasswordHandler}
+        >
+          Forgot your password?
+        </Text>
+      </Flex>
     </Container>
   );
 };
